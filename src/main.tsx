2348cbfe7b23534aa1501ce1c691cab6c2f60685
EthanJ99 <9293417+EthanJ99@users.noreply.github.com>
2025-02-09 22:41:16 +0000

    Remove unused assets

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { FileData } from './components/File.tsx'
-import { FolderData } from './components/Folder.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
