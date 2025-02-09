731a8880545e2cb94dd4d1993ca731b71bd27a4c
EthanJ99 <9293417+EthanJ99@users.noreply.github.com>
2025-02-09 17:59:36 +0000

    Improve app test

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -6,8 +6,22 @@ import App from  "../App";
 describe('App', () => {
   it('display file list', () => {
     render(<App />)
+
+    // 3 lists in data list
+    expect(screen.getAllByRole("list").length === 3);
+    expect(screen.getByText("Employee Handbook")).toBeInTheDocument();
+    expect(screen.getByText("Public Holiday policy")).toBeInTheDocument();
+    expect(screen.getByText("Expenses")).toBeInTheDocument();
+    expect(screen.getByText("Expenses claim form")).toBeInTheDocument();
+    expect(screen.getByText("Fuel allowances")).toBeInTheDocument();
+    expect(screen.getByText("Cost centres")).toBeInTheDocument();
+    expect(screen.getByText("Misc")).toBeInTheDocument();
+    expect(screen.getByText("Christmas party")).toBeInTheDocument();
+    expect(screen.getByText("Welcome to the company!")).toBeInTheDocument();
+
   });
-  //it('open folder', () => {fail()});
+  
+  //it('able to open folder', () => {fail()});
   //it('filter by filename', () => {fail()});
   //it('sort by name', () => {fail()});
   //it('sort by date', () => {fail()});
